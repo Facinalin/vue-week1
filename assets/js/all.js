65f27ab06ee9425afed1fe21dd3da581ff00849d
facina <lovemistborn13@gmail.com>
2023-01-09 17:20:24 +0800

    Update 2023-01-09T09:20:22.480Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,10 @@
 "use strict";
+
+var test = document.querySelector('#test');
+
+if (test) {
+  test.addEventListener('click', function () {
+    console.log('測試點到');
+  });
+}
 //# sourceMappingURL=all.js.map
